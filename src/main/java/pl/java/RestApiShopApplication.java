8afcef2a5d862be3fa40af2f2ab42a1a7201ef58
tinalesniak <tinalesniak1@gmail.com>
2023-01-05 21:05:04 +0100

    initial commit

---
+++ src/main/java/pl/java/RestApiShopApplication.java
@@ -0,0 +1,13 @@
+package pl.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiShopApplication.class, args);
+    }
+
+}
